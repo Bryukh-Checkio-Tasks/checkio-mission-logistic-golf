f75721d85526c5c5f6c93080d67a9148d180cbaa
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-10-06 16:50:50 +0400

    graphic

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,6 +1,6 @@
 //Dont change it
-requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
-    function (ext, $, Raphael, Snap) {
+requirejs(['ext_editor_1', 'jquery_190', 'raphael_212'],
+    function (ext, $, Raphael) {
 
         var cur_slide = {};
 
@@ -8,9 +8,7 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
         });
 
         ext.set_process_in(function (this_e, data) {
-            cur_slide = {};
             cur_slide["in"] = data[0];
-            this_e.addAnimationSlide(cur_slide);
         });
 
         ext.set_process_out(function (this_e, data) {
@@ -19,6 +17,8 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
 
         ext.set_process_ext(function (this_e, data) {
             cur_slide.ext = data;
+            this_e.addAnimationSlide(cur_slide);
+            cur_slide = {};
         });
 
         ext.set_process_err(function (this_e, data) {
@@ -28,8 +28,47 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
         });
 
         ext.set_animate_success_slide(function (this_e, options) {
-            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div></div></div>'));
-            this_e.setAnimationHeight(115);
+            var ends = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"]
+
+            options = options || {};
+            var is_new_record = options.is_new_record || false;
+            var place_rating = String(options.place_rating || 0);
+            var best_points = options.best_points || 0;
+            var current_points = options.current_points || 0;
+            var $div = $("<div></div>");
+            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div class="result"></div></div>'));
+            var $resultDiv = $h.find(".result");
+            var $table = $("<table></table>").addClass("numbers");
+            if (is_new_record) {
+                $resultDiv.addClass("win-sign");
+                $resultDiv.append($("<div></div>").text("You beat your best results!"));
+                var $tr = $("<tr></tr>");
+                $tr.append($("<th></th>").text(best_points));
+                $tr.append($("<th></th>").text(place_rating).append($("<span></span>").addClass(".ends").text(ends[Number(place_rating[place_rating.length - 1])])));
+
+                $table.append($tr);
+                $tr = $("<tr></tr>");
+                $tr.append($("<td></td>").text("Personal best"));
+                $tr.append($("<td></td>").text("Place"));
+                $table.append($tr);
+            }
+            else {
+                $resultDiv.addClass("norm-sign");
+                $resultDiv.append($("<div></div>").text("Your results"));
+                $tr = $("<tr></tr>");
+                $tr.append($("<th></th>").text(current_points));
+                $tr.append($("<th></th>").text(best_points));
+                $tr.append($("<th></th>").text(place_rating).append($("<span></span>").addClass(".ends").text(ends[Number(place_rating[place_rating.length - 1])])));
+
+                $table.append($tr);
+                $tr = $("<tr></tr>");
+                $tr.append($("<td></td>").text("Points"));
+                $tr.append($("<td></td>").text("Personal best"));
+                $tr.append($("<td></td>").text("Place"));
+                $table.append($tr);
+            }
+            $resultDiv.append($table);
+            this_e.setAnimationHeight(255);
         });
 
         ext.set_animate_slide(function (this_e, data, options) {
@@ -40,17 +79,20 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
             }
 
             //YOUR FUNCTION NAME
-            var fname = 'checkio';
+            var fname = 'golf';
 
-            var checkioInput = data.in;
+            var checkioInput = data.in || 1401;
             var checkioInputStr = fname + '(' + JSON.stringify(checkioInput) + ')';
+            var isCall = true;
 
             var failError = function (dError) {
-                $content.find('.call').html(checkioInputStr);
-                $content.find('.output').html(dError.replace(/\n/g, ","));
+                if (isCall) {
+                    $content.find('.call').html('Fail: ' + checkioInputStr);
+                    $content.find('.call').addClass('error');
+                }
 
+                $content.find('.output').html(dError.replace(/\n/g, ","));
                 $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
                 $content.find('.answer').remove();
                 $content.find('.explanation').remove();
                 this_e.setAnimationHeight($content.height() + 60);
@@ -63,44 +105,36 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
 
             if (data.ext && data.ext.inspector_fail) {
                 failError(data.ext.inspector_result_addon);
+                $content.find('.call').remove();
+                isCall = false;
                 return false;
             }
 
-            $content.find('.call').html(checkioInputStr);
-            $content.find('.output').html('Working...');
+            var rightResult = data.ext["answer"];
+            var userResult = data.out;
+            var result = data.ext["result"];
+            var result_addon = data.ext["result_addon"];
 
 
-            if (data.ext) {
-                var rightResult = data.ext["answer"];
-                var userResult = data.out;
-                var result = data.ext["result"];
-                var result_addon = data.ext["result_addon"];
+            //if you need additional info from tests (if exists)
+            var explanation = data.ext["explanation"];
 
-                //if you need additional info from tests (if exists)
-                var explanation = data.ext["explanation"];
-                $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
-                if (!result) {
-                    $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
-                    $content.find('.answer').addClass('error');
-                    $content.find('.output').addClass('error');
-                    $content.find('.call').addClass('error');
-                }
-                else {
-                    $content.find('.answer').remove();
-                }
+            $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
+
+            if (!result) {
+                $content.find('.call').html('Fail: ' + checkioInputStr);
+                $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
+                $content.find('.answer').addClass('error');
+                $content.find('.output').addClass('error');
+                $content.find('.call').addClass('error');
             }
             else {
+                $content.find('.call').html('Pass: ' + checkioInputStr);
                 $content.find('.answer').remove();
             }
 
-
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
+            var canvas = new SVG($content.find(".explanation")[0]);
+            canvas.draw(checkioInput, explanation);
 
 
             this_e.setAnimationHeight($content.height() + 60);
@@ -120,28 +154,78 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
 //                this_e.sendToConsoleCheckiO("something");
 //            });
 //        });
+        function SVG(dom) {
 
-        var colorOrange4 = "#F0801A";
-        var colorOrange3 = "#FA8F00";
-        var colorOrange2 = "#FAA600";
-        var colorOrange1 = "#FABA00";
+            var colorOrange4 = "#F0801A";
+            var colorOrange3 = "#FA8F00";
+            var colorOrange2 = "#FAA600";
+            var colorOrange1 = "#FABA00";
 
-        var colorBlue4 = "#294270";
-        var colorBlue3 = "#006CA9";
-        var colorBlue2 = "#65A1CF";
-        var colorBlue1 = "#8FC7ED";
+            var colorBlue4 = "#294270";
+            var colorBlue3 = "#006CA9";
+            var colorBlue2 = "#65A1CF";
+            var colorBlue1 = "#8FC7ED";
 
-        var colorGrey4 = "#737370";
-        var colorGrey3 = "#9D9E9E";
-        var colorGrey2 = "#C5C6C6";
-        var colorGrey1 = "#EBEDED";
+            var colorGrey4 = "#737370";
+            var colorGrey3 = "#9D9E9E";
+            var colorGrey2 = "#C5C6C6";
+            var colorGrey1 = "#EBEDED";
 
-        var colorWhite = "#FFFFFF";
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
+            var colorWhite = "#FFFFFF";
+
+            var cell = 40;
+            var radius = cell * 0.4;
+
+            var pad = radius * 1.5;
+
+            var size = pad * 2 + cell * 8;
+
+            var paper = Raphael(dom, size, size);
+
+            var aPoint = {"stroke": colorBlue4, "stroke-width": 2, "fill": colorBlue1};
+            var aNumber = {"stroke": colorBlue4, "font-size": radius, "font-family": "Roboto"};
+            var aLine = {"stroke": colorBlue4, "stroke-width": 5};
+            var aLineX = {"stroke": colorOrange4, "stroke-width": 5};
+
+
+            this.draw = function (matrix, data) {
+                var coordinates = data[0];
+                var path = "," + String(data[1]) + ",";
+
+                for (var i = 0; i < coordinates.length; i++) {
+                    paper.circle(pad + (coordinates[i][0] - 1) * cell,
+                        pad + (coordinates[i][1] - 1) * cell, radius).attr(aPoint);
+                    paper.text(pad + (coordinates[i][0] - 1) * cell,
+                        pad + (coordinates[i][1] - 1) * cell, i).attr(aNumber);
+                }
+
+                for (var r = 0; r < matrix.length; r++) {
+                    for (var c = r; c < matrix[r].length; c++) {
+                        if (matrix[r][c] != 0) {
+                            var p = paper.path([
+                                ["M", pad + (coordinates[r][0] - 1) * cell,
+                                    pad + (coordinates[r][1] - 1) * cell],
+                                ["L", pad + (coordinates[c][0] - 1) * cell,
+                                    pad + (coordinates[c][1] - 1) * cell]
+                            ]).attr(aLine);
+                            if (path.indexOf("," + r + "," + c + ",") !== -1 || path.indexOf("," + c + "," + r + ",") !== -1) {
+                                p.attr(aLineX);
+                            }
+                            p.toBack();
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+//Your Additional functions or objects inside scope
+//
+//
+//
 
 
     }
-);
+)
+;
