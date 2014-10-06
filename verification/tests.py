c003fcb079bf9a2b23fd2c86b058fe358b29e07f
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-10-06 08:26:43 +0400

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,21 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [[0, 80, 58, 0], [80, 0, 71, 80], [58, 71, 0, 58], [0, 80, 58, 0]],
+            "answer": 116,
+            "explanation": [[[1, 1], [1, 9], [6, 4], [9, 9]], (0, 2, 3)]
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "input": [[0, 57, 0, 0, 0], [57, 0, 71, 0, 0], [0, 71, 0, 45, 0], [0, 0, 45, 0, 58],
+                      [0, 0, 0, 58, 0]],
+            "answer": 231,
+            "explanation": [[[5, 5], [1, 1], [2, 8], [6, 6], [9, 1]], (0, 1, 2, 3, 4)]
+        },
     ],
     "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+
     ]
 }
