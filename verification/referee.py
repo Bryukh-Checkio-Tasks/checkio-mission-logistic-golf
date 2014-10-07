55bdc14be9d91475d00b5360d806bc798b16bec5
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-10-07 16:48:52 +0400

    cover

--- verification/referee.py
+++ verification/referee.py
@@ -5,10 +5,18 @@ from golf import CheckioRefereeGolf
 from tests import TESTS
 
 
+cover = """def cover(f, data):
+    return f(tuple(tuple(row) for row in data))
+"""
+
 api.add_listener(
     ON_CONNECT,
     CheckioRefereeGolf(
         max_length=350,
         tests=TESTS,
-        function_name="golf"
+        function_name="golf",
+        cover_code={
+            'python-27': cover,
+            'python-3': cover,
+        },
     ).on_ready)
